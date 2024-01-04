5a26f8013496f7c1c1c0a850eb1e22fce7239ab2
Alec Jensen <alecbradyjensen@gmail.com>
2024-01-03 20:53:38 -0600

    anotha one

--- kidney-bot/cogs/other.py
+++ kidney-bot/cogs/other.py
@@ -29,7 +29,7 @@ class Other(commands.Cog):
     @app_commands.command(name='idk', description='Alec said IDK when I asked him what to make so I said kk and here it is')
     async def idk(self, interaction: discord.Interaction):
         emb = discord.Embed(color=0x313338)
-        emb.set_image(url="https:///prosurestring.github.io/alecidk.jpg.png")
+        emb.set_image(url="https://www.prosurestring.xyz/alecidk.png")
         await interaction.response.send_message(embed=emb, ephemeral=True)
 
     @app_commands.command(name='ping', description='Get the current ping of the bot.')
